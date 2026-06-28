74d1b16a7c1d2c0f4360962c627ce4c6597cd043
wlyyy5 <wlyyy55@gmail.com>
2026-06-28 08:32:10 +0800

    Update 111.py

--- 111.py
+++ 111.py
@@ -10,9 +10,13 @@ numbers += [3,8]
 print(len(numbers))
 print(max(numbers))
 #几个数字、最大数字
+
 print(numbers[1:])
 #从第一个数字开始到最后一个数字。
+
 print ("numbers[1:-2]: ", numbers[1:-2])
+#全部数字[1, 5, 9, 2, 4, 5, 3, 8]
+#结果：numbers[1:-2]:  [5, 9, 2, 4, 5]
 
 print(numbers.count(5))
 #统计某个元素在列表中出现的次数
