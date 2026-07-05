2ea844aa2837d9a165a027b10b6cdff1e7299415
wlyyy5 <wlyyy55@gmail.com>
2026-07-05 10:53:34 +0800

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -6,3 +6,12 @@ print("How much do you weight?",end='')
 weight = input()
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
+#课后作业，用x = int(input())改后的。
+
+age = int(input("How old are you?"))
+height = int(input("How tall are you?"))
+weight = int(input("How much do you weigh?"))
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
