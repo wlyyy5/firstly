0556a0fa737f9f7cf3bfde82ac42b927049361a3
wlyyy5 <wlyyy55@gmail.com>
2026-07-08 07:09:45 +0800

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -6,3 +6,4 @@ print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+#什么意思不太懂啊？怎么按照他说的出不来
